a8052dcfaff7ed09bbc7c1029d3c200cdd9059c9
Govind Hari <173696360+gh4rial@users.noreply.github.com>
2024-10-10 22:52:34 +0530

    Initial commit

---
+++ gl_paint.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+def main():
+    root = Tk()
+    root.title("a window")
+    # mainframe = ttk.Frame(root, padding="3 3 12 12")
+    # mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    # root.columnconfigure(0, weight=1)
+    # root.rowconfigure(0, weight=1)
+
+    # root.mainloop()
+    
+main()
